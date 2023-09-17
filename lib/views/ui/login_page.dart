2ee692d2ae57595886276d88e056da1b60286446
James Angelo Sarona <jacsarona.official@gmail.com>
2023-09-17 14:38:23 +0800

    added login page and functionality

---
+++ lib/views/ui/login_page.dart
@@ -0,0 +1,179 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:nutri_gabay_admin/views/shared/app_style.dart';
+import 'package:nutri_gabay_admin/views/shared/custom_buttons.dart';
+import 'package:nutri_gabay_admin/views/shared/custom_text_fields.dart';
+import 'package:nutri_gabay_admin/views/ui/home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late Size screenSize;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _username = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  bool isObscure = true;
+  String loginMsg = '';
+
+  void validation() async {
+    loginMsg = '';
+    if (_formKey.currentState!.validate()) {
+      final collection = FirebaseFirestore.instance.collection('admin');
+
+      collection
+          .where('username', isEqualTo: _username.text)
+          .where('password', isEqualTo: _password.text)
+          .get()
+          .then((querySnapshot) {
+        if (querySnapshot.size > 0) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => const HomePage()),
+          );
+        } else {
+          loginMsg = 'Incorrect username or password';
+        }
+        setState(() {});
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: SizedBox(
+        height: screenSize.height,
+        width: screenSize.width,
+        child: ListView(
+          children: [
+            Form(
+              key: _formKey,
+              child: Container(
+                margin:
+                    EdgeInsets.symmetric(horizontal: screenSize.width * 0.4),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: screenSize.height * 0.15,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
+                          height: 60,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("assets/images/logo-name.png"),
+                              fit: BoxFit.fitHeight,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          'Welcome Admin',
+                          style: appstyle(
+                            20,
+                            Colors.black,
+                            FontWeight.w800,
+                          ).copyWith(
+                            shadows: const <Shadow>[
+                              Shadow(
+                                offset: Offset(-1, 10),
+                                blurRadius: 3.0,
+                                color: Colors.grey,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 50),
+                    Text(
+                      "Username",
+                      style: appstyle(
+                        12,
+                        Colors.black,
+                        FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    UserCredentialTextField(
+                      controller: _username,
+                      label: "",
+                      isObscure: false,
+                      keyboardType: TextInputType.text,
+                      validation: (value) {
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      "Password",
+                      style: appstyle(
+                        12,
+                        Colors.black,
+                        FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    UserCredentialTextField(
+                      controller: _password,
+                      label: "",
+                      isObscure: isObscure,
+                      keyboardType: TextInputType.text,
+                      validation: (value) {
+                        return null;
+                      },
+                      icon: isObscure
+                          ? FontAwesomeIcons.eye
+                          : FontAwesomeIcons.eyeSlash,
+                      onTap: () {
+                        setState(() {
+                          isObscure = !isObscure;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      loginMsg,
+                      style: appstyle(
+                        11,
+                        Colors.red,
+                        FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 80),
+                      width: double.infinity,
+                      height: 40,
+                      child: UserCredentialPrimaryButton(
+                        onPress: () {
+                          validation();
+                        },
+                        label: "Login",
+                        labelSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
