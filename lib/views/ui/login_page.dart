d54a0db8519ce842a642ee7fe82529ac80e2972e
James Angelo Sarona <jacsarona.official@gmail.com>
2023-09-22 23:00:34 +0800

    test auto deploy

--- lib/views/ui/login_page.dart
+++ lib/views/ui/login_page.dart
@@ -78,7 +78,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.blue,
       body: SizedBox(
         height: double.infinity,
         width: double.infinity,
